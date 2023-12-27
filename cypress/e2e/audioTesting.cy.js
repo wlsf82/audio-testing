b18a3bb9bc0185080db89bf88dae03ad81ec6ba7
Walmyr <wlsf82@gmail.com>
2023-12-27 19:55:08 +0100

    Implement audio testing

--- cypress/e2e/audioTesting.cy.js
+++ cypress/e2e/audioTesting.cy.js
@@ -2,6 +2,38 @@ describe('cy.handsOn("Audio Testing")', () => {
   beforeEach(() => cy.visit('./src/index.html'))
 
   it('ensures audio plays and pauses', () => {
+    cy.contains('button', 'Play').click()
+
+    cy.get('#joker-remix', { log: false })
+      .as('jokerRemix')
+      .should(audioElements => {
+        let audible = false
+
+        if (
+          audioElements[0].duration > 0 &&
+          audioElements[0].played &&
+          !audioElements[0].paused &&
+          !audioElements[0].muted
+        ) {
+          audible = true
+        }
+
+        expect(audible).to.be.equal(true)
+      })
+
+    cy.contains('button', 'Pause')
+      .click()
+      .blur()
+
+    cy.get('@jokerRemix', { log: false })
+      .should(audioElements => {
+        let audible = true
+
+        if (audioElements[0].paused || audioElements[0].muted) {
+          audible = false
+        }
     
+        expect(audible).to.be.equal(false)
+      })
   })
 })
